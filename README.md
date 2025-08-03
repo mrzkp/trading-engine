78790ebe6f099af8b37418dded3d8c64e0b06270
Aditya Tummala <adi.tummala@berkeley.edu>
2025-08-02 19:35:05 -0700

    docs

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Trading Engine
 
-### Features
+## Terminology
 
+__Exchange__: The specific platform we we pulling data from.
+__Market__: The specified trades available on that platform (e.g., ETH/BTC). Includes what you can actually trade and includes details such as prices, limits, and precisions.
+
+__prices__: When we fetch them from ccxt, we are getting the previous and current trading rates for a market. To get current prices, we do fetch_ticker('BTC/USDT') and to get, say, the previous hours values, we can do something like fetch_ohlcv('BTC/USDT', '1h'). These are not stored statically within
+__limits__:
+__fees__:
+__precisions__:
+
+### Features
 * Arbitrage - Order Execution
 * Threshold L2 Order Book Depth - Order Execution
-
 
 NOTES: Activate venv first:
 python3 -m venv venv
--- arbitrage/test.py
+++ arbitrage/test.py
@@ -8,24 +8,36 @@ Test for reloading markets.
 def main():
     try:
         poloniex = ccxt.poloniex({'verbose': True}) # log HTTP requests`
-        poloniex.load_markets() # request markets
-        print(poloniex.id, poloniex.markets)   # output a full list of all loaded markets
+        print(poloniex.has) # Fetches methods
 
-        print(list(poloniex.markets.keys())) # output a short list of market symbols
-        print(poloniex.markets['SEN/USDT'])     # output single market details
-        poloniex.load_markets() # return a locally cached version, no reload
 
-        print("\n\n\n\n\n")
-        print("\n\n\n\n\n")
-        print("\n\n\n\n\n")
-        print("\n\n\n\n\n")
-        reloadedMarkets = poloniex.load_markets(True) # force HTTP reload = True
+        # poloniex.load_markets() # Gets markets
+        # print(poloniex.id, poloniex.markets) # output a full list of all loaded markets
 
-        print("\n\n\n\n\n")
-        print("\n\n\n\n\n")
-        print("\n\n\n\n\n")
-        print("\n\n\n\n\n")
-        print(reloadedMarkets['HONKETH/USDT'])
+        # print(list(poloniex.markets.keys())) # output a short list of market symbols
+        # print(poloniex.markets['SEN/USDT'])     # output single market details
+        # poloniex.load_markets() # return a locally cached version, no reload, as we have already loaded earlier.
+        #
+        # This has some consequences. Doesn't use an API request, however, as a result, we aren't getting the most recent information. Is it possible to get a PARTICULAR market's most recent updates?
+
+        # print("\n\n\n\n\n")
+        # print("\n\n\n\n\n")
+        # print("\n\n\n\n\n")
+        # print("\n\n\n\n\n")
+        # reloadedMarkets = poloniex.load_markets(True) # force HTTP reload = True
+        #
+        # This uses an API request, gets the most recent information.
+        #
+
+        """
+        Note: If storing information locally, all objects are stored
+        """
+
+        # print("\n\n\n\n\n")
+        # print("\n\n\n\n\n")
+        # print("\n\n\n\n\n")
+        # print("\n\n\n\n\n")
+        # print(reloadedMarkets['HONKETH/USDT'])
 
     except KeyError as KE:
         print(KE)
